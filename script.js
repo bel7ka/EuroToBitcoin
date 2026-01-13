411e602896aa93c0482a54a0a4e445e530967225
Djelil_Belkacem <belkacem.djelil@edouardgand.fr>
2026-01-13 11:15:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function convertir() {
+    let euro = document.getElementById("euro").value;
+
+    // Taux au 10/01/2026 : 1 BTC = 77 600 €
+    let taux = 77600;
+
+    // BUG : la formule est incorrecte
+    let bircoin = euro * taux;
+
+    document.getElementById("resultat").textContent = 
+        euro + " € = " + bitcoin + " BTC ";
+}
