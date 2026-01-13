135ea977f6ea88f7ffc7af27e9e6d521aba6df86
q-gaillard <quentin.gaillard@edouardgand.fr>
2026-01-13 11:28:42 +0100

    modification taux 3

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function convertir() {
     let euro = document.getElementById("euro").value;
 
     // Taux au 10/01/2026 : 1 BTC = 77 600 €
-    let taux = 78959;
+    let taux = 77600;
 
-    let bitcoin = euro * taux;
+    // BUG : la formule est incorrecte
+    let bircoin = euro / taux;
 
     document.getElementById("resultat").textContent = 
         euro + " € = " + bitcoin + " BTC ";
